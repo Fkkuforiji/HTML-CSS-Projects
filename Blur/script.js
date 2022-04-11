47092cf597a832f75939bf941434e8ec0c78c46d
Fkkuforiji <fkkuforiji@gmail.com>
2022-04-11 16:47:20 -0400

    Began Vertical Slider Project

--- Blur/script.js
+++ Blur/script.js
@@ -1,22 +1,31 @@
-const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('bg')
+const loadText = document.querySelector(".loading-text");
+const bg = document.querySelector(".bg");
 
-let load = 0
+let load = 0;
 // the 30 is 30 milliseconds not 30 seconds
-let int = setInterval(blurring, 30)
+let int = setInterval(blurring, 30);
 
-function blurring (){
- // load is increasing by 1 increment
- load++
+function blurring() {
+  // load is increasing by 1 increment
+  load++;
 
- // without this limiter, the function will run infinitely
- if (load > 99) {
-  // test to see what happens if this clearinterval is gone 
-  clearInterval(int)
- }
-// backticks " ` " are needed for template literals
-// innerText refers to the HTML content inside the HTML element of the constant before the .innerText
-  loadText.innerText = `${load}%`
-  loadText.style.opacity m k ;.l
-  
+  // without this limiter, the function will run infinitely
+  if (load > 99) {
+    // test to see what happens if this clearinterval is gone
+    clearInterval(int);
+  }
+  // backticks " ` " are needed for template literals
+  // innerText refers to the HTML content inside the HTML element of the constant before the .innerText
+  //load is referring to the let load above
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  //loadText.style.opacity = scale(100 - load / 100); --can also do opacity this way
+
+  //blur is referring to the blur attribute in the bg css section
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
